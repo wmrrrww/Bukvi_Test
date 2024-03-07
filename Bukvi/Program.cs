971b4fa90247d663ee0ac906e40bcd82a11c7c08
Julia <Julia@WMRRWW>
2024-03-07 08:40:07 +0300

    Добавьте файлы проекта.

---
+++ Bukvi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace отладка28._02
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] letters = { 'f', 'r', 'e', 'd', ' ', 's', 'm', 'i', 't', 'h' };
+            string name = "";
+            int[] a = new int[10];
+            for (int i = 0; i < letters.Length; i++)
+            {
+                name += letters[i];
+                a[i] = i + 1;
+                SendMessage(name, a[i]);
+            }
+            Console.ReadKey();
+        }
+
+        static void SendMessage(string name, int msg)
+        {
+            Console.WriteLine("Hello, " + name + "! Count to " + msg);
+        }
+    }
+}
+
+
